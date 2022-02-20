87791411cb13a10dfd372d2cc7fd4fe4343a766c
SeongHoKim <rlatjdgh9612@naver.com>
2022-02-20 20:20:47 +0900

    Initial Commit

---
+++ FaceID_Authentication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaceID_Authentication
+//
+//  Created by SeongHoKim on 2022/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FaceID_Authentication/FaceID_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceID_AuthenticationApp.swift
+//  FaceID_Authentication
+//
+//  Created by SeongHoKim on 2022/02/20.
+//
+
+import SwiftUI
+
+@main
+struct FaceID_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
